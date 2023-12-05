c45ddf7d2152437837354a61b9d8e21415f7e512
Muhammad Umar <muhammadumar03124701209@gmail.com>
2023-12-05 18:06:29 +0500

    Add new URL to api.js file

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -3,6 +3,8 @@ import axios from "axios";
 // const url = "http://localhost:8000";
 const url = "https://flipcart-bk.onrender.com";
 
+// new url added to the file
+
 export const authenticateLogin = async (user) => {
   try {
     return await axios.post(`${url}/login`, user);
