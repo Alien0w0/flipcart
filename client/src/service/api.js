aade19899f7f81fd4f9ad78515dc2feb6b64671c
Muhammad Umar <muhammadumar03124701209@gmail.com>
2023-12-05 17:26:12 +0500

    Refactor code to improve performance and readability

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const url = "http://localhost:8000";
+// const url = "http://localhost:8000";
+const url = "https://flipcart-bk.onrender.com";
 
 export const authenticateLogin = async (user) => {
   try {
